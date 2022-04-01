ce645eb6093f4b30de8b8a245587070ac8c375a0
Kevin Helliwell <kbh789@gmail.com>
2022-04-01 14:39:59 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,55 +1,33 @@
 import requests
 import json
-response_API = requests.get("https://raider.io/api/v1/characters/profile?region=us&realm=Zul'jin&name=Konnenh&fields=mythic_plus_best_runs")
-data = response_API.text
-parse_json = json.loads(data)
+response_API_best = requests.get("https://raider.io/api/v1/characters/profile?region=us&realm=Zul'jin&name=Konnenh&fields=mythic_plus_best_runs")
+data_best = response_API_best.text
+parse_json_best = json.loads(data_best)
 
-scores = []
+def get_info_best():
+    for i in range(0,10):
+        name = parse_json_best["mythic_plus_best_runs"][i]["short_name"]
+        level = parse_json_best["mythic_plus_best_runs"][i]["mythic_level"]
+        clear = parse_json_best["mythic_plus_best_runs"][i]["clear_time_ms"]
+        par = parse_json_best["mythic_plus_best_runs"][i]["par_time_ms"]
+        score = parse_json_best["mythic_plus_best_runs"][i]["score"]
+        affixes = parse_json_best["mythic_plus_best_runs"][i]["affixes"][0]["name"]
+        print(name,level,clear,par,score,affixes)
 
-for i in range(10):
-    my_best_runs = parse_json["mythic_plus_best_runs"][i]["score"]
-    scores.append(my_best_runs)
-print(scores)
+get_info_best()
 
+response_API_alt = requests.get("https://raider.io/api/v1/characters/profile?region=us&realm=Zul'jin&name=Konnenh&fields=mythic_plus_alternate_runs")
+data_alt = response_API_alt.text
+parse_json_alt = json.loads(data_alt)
 
+def get_info_alt():
+    for i in range(0,10):
+        name = parse_json_alt["mythic_plus_alternate_runs"][i]["short_name"]
+        level = parse_json_alt["mythic_plus_alternate_runs"][i]["mythic_level"]
+        clear = parse_json_alt["mythic_plus_alternate_runs"][i]["clear_time_ms"]
+        par = parse_json_alt["mythic_plus_alternate_runs"][i]["par_time_ms"]
+        score = parse_json_alt["mythic_plus_alternate_runs"][i]["score"]
+        affixes = parse_json_alt["mythic_plus_alternate_runs"][i]["affixes"][0]["name"]
+        print(name,level,clear,par,score,affixes)
 
-
-
-# Dungeons in order (DOS, HOA, MOTS, NW, PF, SD, SOA, TOP, TSOW, TSG)
-# dungeon_times = [43, 31, 30, 36, 38, 41, 39, 37, 38, 30]
-
-# dungeon_dict = {
-#     "DOS":43,
-#     "HOA":31,
-#     "MOTS":30,
-#     "NW":36,
-#     "PF":38,
-#     "SD":41,
-#     "SOA":39,
-#     "TOP":37,
-#     "TSOW":38,
-#     "TSG":30,
-# }
-
-# def baseline_completion_score(key_level):
-    
-#     if(type(key_level)!=int or key_level<2):
-#         return "Please enter a valid key level."
-        
-#     elif(key_level>=2 and key_level<5):
-#         return 37.5 + 7.5*1 + 7.5*key_level
-    
-#     elif(key_level<=5 and key_level<7):
-#         return 37.5 + 7.5*2 + 7.5*key_level
-    
-#     elif(key_level<=7 and key_level<10):
-#         return 37.5 + 7.5*3 + 7.5*key_level
-    
-#     else:
-#         return 37.5 + 7.5*3 + 15 + 7.5*key_level
-
-# # for i in range(2,16):
-# #     print(baseline_completion_score(i))
-
-# def timer_score(time_completed, dungeon):
-        
+get_info_alt()
